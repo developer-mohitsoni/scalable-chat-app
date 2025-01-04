2c4cfb290ea5004db59f291604d24aff6a942a0c
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-04 18:17:32 +0530

    Enhance SocketProvider to include sendMessage functionality

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,15 +1,27 @@
 "use client";
 
-import React from "react";
+import React, { useCallback } from "react";
 
 interface SocketProviderProps {
   children?: React.ReactNode;
 }
 
-const SocketContext = React.createContext(null);
+interface ISocketContext {
+  sendMessage: (msg: string) => void;
+}
+
+const SocketContext = React.createContext<ISocketContext | null>(null);
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+  const sendMessage: ISocketContext["sendMessage"] = useCallback(
+    (msg: string) => {
+      console.log("Send Message: ", msg);
+    },
+    []
+  );
   return (
-    <SocketContext.Provider value={null}>{children}</SocketContext.Provider>
+    <SocketContext.Provider value={{ sendMessage }}>
+      {children}
+    </SocketContext.Provider>
   );
 };
