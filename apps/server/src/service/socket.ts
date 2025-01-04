6e0fa80a9b1d05b91ceca119cdfff92a6e15374a
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-04 18:04:44 +0530

    Initializing Socket Listeners

--- apps/server/src/service/socket.ts
+++ apps/server/src/service/socket.ts
@@ -7,6 +7,19 @@ class SocketService {
     this._io = new Server();
   }
 
+  // Hanlding Socket Events
+  public initListeners() {
+    console.log("Initialize Socket Listeners...");
+    const io = this._io;
+    io.on("connect", (socket) => {
+      console.log("New Socket Connected!", socket.id);
+
+      socket.on("event:message", async ({ message }: { message: string }) => {
+        console.log("New Message Received: ", message);
+      });
+    });
+  }
+
   // Getter Method
   get io() {
     return this._io;
