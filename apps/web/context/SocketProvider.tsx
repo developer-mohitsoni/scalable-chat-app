2ab3304a8715d1425ebece51c989efa56ed71654
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-04 20:09:16 +0530

    Add socket state management to SocketProvider for message sending

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,7 +1,7 @@
 "use client";
 
-import React, { useCallback, useContext, useEffect } from "react";
-import { io } from "socket.io-client";
+import React, { useCallback, useContext, useEffect, useState } from "react";
+import { io, Socket } from "socket.io-client";
 
 interface SocketProviderProps {
   children?: React.ReactNode;
@@ -23,18 +23,26 @@ export const useSocket = () => {
 };
 
 export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
+  const [socket, setSocket] = useState<Socket>();
   const sendMessage: ISocketContext["sendMessage"] = useCallback(
     (msg: string) => {
       console.log("Send Message: ", msg);
+
+      if (socket) {
+        socket.emit("event:message", { message: msg });
+      }
     },
-    []
+    [socket]
   );
 
   useEffect(() => {
     const _socket = io("http://localhost:8000");
 
+    setSocket(_socket);
+
     return () => {
       _socket.disconnect();
+      setSocket(undefined);
     };
   }, []);
   return (
