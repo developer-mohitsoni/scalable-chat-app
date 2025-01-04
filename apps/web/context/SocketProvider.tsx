709056444a02d094364b159dd6dd745ce50df015
Mohit Soni <developer_mohitsoni@outlook.com>
2025-01-04 18:23:23 +0530

    Add socket connection initialization in SocketProvider that establish a connection between client and server

--- apps/web/context/SocketProvider.tsx
+++ apps/web/context/SocketProvider.tsx
@@ -1,6 +1,7 @@
 "use client";
 
-import React, { useCallback } from "react";
+import React, { useCallback, useEffect } from "react";
+import { io } from "socket.io-client";
 
 interface SocketProviderProps {
   children?: React.ReactNode;
@@ -19,6 +20,14 @@ export const SocketProvider: React.FC<SocketProviderProps> = ({ children }) => {
     },
     []
   );
+
+  useEffect(() => {
+    const _socket = io("http://localhost:3000");
+
+    return () => {
+      _socket.disconnect();
+    };
+  }, []);
   return (
     <SocketContext.Provider value={{ sendMessage }}>
       {children}
